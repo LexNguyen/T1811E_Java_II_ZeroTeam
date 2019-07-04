c05f3bfb8cd6e64185da320c8bfd183ea1019208
QuangTrung6620 <50941864+QuangTrung6620@users.noreply.github.com>
2019-07-04 11:32:02 +0700

    BT526

---
+++ Nguyen_Quang_Trung/Bai_Tap/BT526/Student.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class Student {
+    String name;
+    String gender;
+    int rollNo;
+    String email;
+    String address;
+
+    public Student() {
+    }
+
+    public Student(String name, String gender, int rollNo, String email, String address) {
+        this.name = name;
+        this.gender = gender;
+        this.rollNo = rollNo;
+        this.email = email;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    
+}
